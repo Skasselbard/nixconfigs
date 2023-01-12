1a6bbeba8c9c5ef93c2ee81e195026eaf69580ec
tom <tom.meyer89@gmail.com>
2023-01-12 06:40:08 +0100

    static ip error

--- machines/physical_host_base.nix
+++ machines/physical_host_base.nix
@@ -12,7 +12,7 @@ with types; {
   config = {
     networking = {
       hostName = config.hostname;
-      interfaces = if config.ip == null && config.interface == null then {
+      interfaces = if config.ip != null && config.interface != null then {
         "${config.interface}" = {
           ipv4.addresses = [{
             address = config.ip;
@@ -21,11 +21,6 @@ with types; {
         };
       } else
         { };
-
-      defaultGateway = {
-        address = "192.168.1.1"; # router address
-        interface = "${config.interface}"; # go to other nets through the bridge
-      };
     };
   };
 }
