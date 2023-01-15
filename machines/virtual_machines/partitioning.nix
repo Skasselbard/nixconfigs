ed1d52b7a0f53e4680792f0a813aabacb6b45da9
tom <tom.meyer89@gmail.com>
2023-01-15 19:51:16 +0100

    removed vm special partitions

--- machines/virtual_machines/partitioning.nix
+++ machines/virtual_machines/partitioning.nix
@@ -1,15 +1,1 @@
-{
-  fileSystems."/".device = "/dev/disk/by-label/nixos";
-  fileSystems."/etc/nixos".device = "/dev/vdb1";
-  fileSystems."/etc/nixos/hosts/self" = {
-    device = "/dev/vdc1";
-    fsType = "vfat";
-    depends = ["etc/nixos"];
-    options = [ "ro" "nofail" ];
-  };
-  fileSystems."/root/.ssh" = {
-    device = "/dev/vdd1";
-    fsType = "vfat";
-    options = [ "ro" "nofail" ];
-  };
-}
+{ fileSystems."/".device = "/dev/disk/by-label/nixos"; }
