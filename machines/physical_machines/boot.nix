0a8c7ba2a46b2d0c8fa52f02cc429a82cfc8e0f3
tom <tom.meyer89@gmail.com>
2023-01-20 06:48:26 +0100

    added default config to physical hosts

--- machines/physical_machines/boot.nix
+++ machines/physical_machines/boot.nix
@@ -2,10 +2,7 @@
   boot = {
     loader.systemd-boot.enable = true;
     loader.efi.canTouchEfiVariables = true;
-    # loader.efi.efiSysMountPoint = "/boot/efi";
     kernelModules = [ "kvm-amd" "kvm-intel" ];
-    # Setup keyfile
-    # initrd.secrets = { "/crypto_keyfile.bin" = null; };
     initrd.availableKernelModules = [
       "ehci_pci"
       "ahci"
--- machines/physical_machines/default.nix
+++ machines/physical_machines/default.nix
@@ -1,8 +1,10 @@
-{... }:
-let nixos_libvirt = 
-  builtins.fetchGit {url="https://github.com/Skasselbard/NixOs-Libvirt";}; 
+{ ... }:
+let
+  nixos_libvirt =
+    builtins.fetchGit { url = "https://github.com/Skasselbard/NixOs-Libvirt"; };
 in {
   imports = [
+    ../default.nix
     nixos_libvirt.outPath
     ./boot.nix
     ./partitioning.nix
