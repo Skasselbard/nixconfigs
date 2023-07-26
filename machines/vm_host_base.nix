a833b955bb5ca0906c3eda0dd58b4ef30e38d088
tom <tom.meyer89@gmail.com>
2023-07-26 07:37:56 +0200

    update vms

--- machines/vm_host_base.nix
+++ machines/vm_host_base.nix
@@ -5,23 +5,4 @@
     ./virtual_machines/boot.nix
     ../default.nix
   ];
-
-  sshKey = builtins.readFile (builtins.head
-    (builtins.filter builtins.pathExists [ "/etc/hive/ssh/id_rsa.pub" ]));
-
-  systemd.services.nixos_configure = {
-    path = with pkgs; [ nixos-rebuild git ];
-    environment = {
-      NIX_PATH = "nixpkgs=/nix/var/nix/profiles/per-user/root/channels/nixos";
-    };
-    wantedBy = [ "multi-user.target" ];
-    serviceConfig = {
-      Type = "oneshot";
-      RemainAfterExit = "yes";
-    };
-    script = ''
-      nixos-rebuild boot -I nixos-config=/etc/nixos/hive/self/configuration.nix
-      reboot
-    '';
-  };
 }
