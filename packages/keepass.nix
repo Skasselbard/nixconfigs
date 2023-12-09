0d1f35b22264cb2366bcdaf74a343b325c672dea
Tom Meyer <tom.meyer89@gmail.com>
2023-12-09 11:25:09 +0100

    keepass

--- packages/keepass.nix
+++ packages/keepass.nix
@@ -2,15 +2,11 @@
   environment.systemPackages = [ pkgs.keepassxc ];
   services.gnome.gnome-keyring.enable = lib.mkForce false;
   programs.ssh.startAgent = true;
+  security.pam.enableSSHAgentAuth = true;
   home-manager-desktop.services.gnome-keyring.enable = false;
   home-manager-desktop.home.file."/.config/keepassxc/keepassxc.ini" = {
     onChange =
       "cp /home/$USER/.config/keepassxc/keepassxc.ini /home/$USER/repos/nixconfigs/packages/configs";
     source = ./configs/keepassxc.ini;
   };
-
-
-  # security.pam.enableSSHAgentAuth
-  # Enable sudo logins if the user’s SSH agent provides a key present in ~/.ssh/authorized_keys. This allows machines to exclusively use SSH keys instead of passwords.
-
 }
