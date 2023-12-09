3503d2ff82ae1afa7d984420484efebaf919a394
Tom Meyer <tom.meyer89@gmail.com>
2023-12-09 09:34:04 +0100

    keepass

--- packages/keepass.nix
+++ packages/keepass.nix
@@ -1,5 +1,6 @@
 { pkgs, lib, config, ... }: {
   environment.systemPackages = [ pkgs.keepassxc ];
+  services.gnome.gnome-keyring.enable = false;
   home-manager-desktop.services.gnome-keyring.enable = false;
   home-manager-desktop.home.file."/config/keepassxc.ini" = {
     # onChange = "";
