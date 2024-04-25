27b52f483de02b210bb0886a2949cbe87598a38b
Tom Meyer <tom.meyer89@gmail.com>
2024-04-25 09:42:55 +0200

    formatted flake.nix

--- flake.nix
+++ flake.nix
@@ -7,17 +7,21 @@
   };
 
   outputs = { self, nixpkgs, home-manager, crane, ... }: {
-    nixosModules = with import nixpkgs { system = "x86_64-linux"; };
+    nixosModules = 
+      with import nixpkgs { system = "x86_64-linux"; };
       with pkgs.lib; {
+
         default = { config, ... }: {
           imports = [
             "${nixpkgs}/nixos/modules/profiles/minimal.nix"
             "${self}/default.nix"
           ];
         };
+
         users = { config, ... }: {
           imports = [ "${self}/usesrs/default.nix" ];
         };
+
         resolveRequirements = { userConfigs, pkgs }:
           (concatMap (module:
             # IMPORTANT: This import requires that every user module is a function that can be called
@@ -28,19 +32,19 @@
               lib = pkgs.lib;
             }).requiredSystemModules)
             (concatMap (userConfig: userConfig.homeModules) userConfigs));
+
         userConfig = import "${self}/users/userConfig.nix";
+
         defaultAdmin = import "${self}/users/defaultAdmin.nix";
-        packages.desktop = {
-          imports = [ "${self}/packages/system/desktop.nix" ];
+
+        packages = {
+          desktop = { imports = [ "${self}/packages/system/desktop.nix" ]; };
+          dns = { imports = [ "${self}/packages/system/dns.nix" ]; };
+          keepass = { imports = [ "${self}/packages/system/keepass.nix" ]; };
+          steam = { imports = [ "${self}/packages/system/steam.nix" ]; };
+          prometheus = { imports = [ "${self}/packages/prometheus.nix" ]; };
         };
-        packages.dns = { imports = [ "${self}/packages/system/dns.nix" ]; };
-        packages.keepass = {
-          imports = [ "${self}/packages/system/keepass.nix" ];
-        };
-        packages.steam = { imports = [ "${self}/packages/system/steam.nix" ]; };
-        packages.prometheus = {
-          imports = [ "${self}/packages/prometheus.nix" ];
-        };
-      };
+
+     };
   };
 }
